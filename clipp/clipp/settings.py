0b8a1c178b80e88492605c34fb1607409bafe79e
Muhammad Tahir <tahirs95@hotmail.com>
2019-04-29 13:14:25 +0500

    Update settings.py

--- clipp/clipp/settings.py
+++ clipp/clipp/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'mqq_!5c98suhid9g&tltpxwr9&g#oe2xua2hxo6lt^vfc87gdu'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
